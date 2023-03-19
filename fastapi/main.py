a4b299ac09c98b3af0b1eb98cb15159b8481f564
sciooga <sciooga@gmail.com>
2023-03-19 05:46:06 -0400

    init

---
+++ fastapi/main.py
@@ -0,0 +1,223 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from bson import ObjectId
+from model import *
+import datetime
+import aiohttp
+import random
+import re
+
+
+app = FastAPI(
+    title="V2EX Plus 扩展后端服务",
+    description='为扩展提供 vDaily 数据支撑',
+    version="1.0.0",
+    terms_of_service="https://chrome.google.com/webstore/detail/v2ex-plus/daeclijmnojoemooblcbfeeceopnkolo",
+    contact={
+        "name": "sciooga",
+        "url": "https://huguotao.com",
+        "email": "sciooga@gmail.com",
+    },
+    license_info={
+        "name": "GPL-3.0",
+        "url": "https://www.gnu.org/licenses/gpl-3.0.en.html",
+    },)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+client = MongoClient('mongodb://mongo')
+db = client.V2EX
+
+
+@app.get("/")
+async def home():
+    '''各类主题列表及爬虫信息'''
+    # db.reply.delete_many({'content': {'$regex': r'div class="show-reply"'}})
+
+    topic_recent_90days_total = db.topic.count_documents({
+        'spiderTime': {'$lte': datetime.datetime.now() - datetime.timedelta(days=90)}
+    })
+
+    latest_topic = db.topic.find_one(sort=[('id', -1)])
+    latest_topic_id = latest_topic['id']
+
+    info = db.info.find_one()
+    cursor = info['cursor']
+
+    # 任务总数
+    task_total = db.task.count_documents({})
+    # 未分配任务总数
+    task_not_distribute_total = db.task.count_documents({
+        'distribute_time': None
+    })
+    # 未完成任务总数
+    task_not_complete_total = db.task.count_documents({
+        'complete_time': None
+    })
+    # 分配后超时 1 分钟未完成任务总数
+    task_distribute_but_not_complete_total = db.task.count_documents({
+        'distribute_time': {'$lte': datetime.datetime.now() - datetime.timedelta(seconds=60)},
+        'complete_time': None
+    })
+    # N 待处理错误
+    error_total = db.error.count_documents({})
+
+    return {
+        'topic_recent_90days_total': topic_recent_90days_total,
+        'latest_topic_id': latest_topic_id,
+        'cursor': cursor,
+        'doc': '/redoc',
+        'task_total': task_total,
+        'task_not_distribute_total': task_not_distribute_total,
+        'task_not_complete_total': task_not_complete_total,
+        'task_distribute_but_not_complete_total': task_distribute_but_not_complete_total,
+        'error_total': error_total,
+    }
+
+
+@app.get("/api/topic/recommend", response_model=List[Topic])
+async def topic_recommend() -> List[Topic]:
+    '''推荐主题'''
+    topics = db.topic.find({
+        'date': {
+            '$gte': datetime.datetime.now() - datetime.timedelta(days=3)
+        }
+    }).sort("score", -1).limit(30)
+    topics = random.sample(list(topics), 10)
+    return list(map(dict, topics))
+
+
+@app.get("/api/reply/recommend", response_model=List[Reply])
+async def topic_recommend() -> List[Reply]:
+    '''推荐回复'''
+    replys = db.reply.find({
+        'date': {
+            '$gte': datetime.datetime.now() - datetime.timedelta(days=3)
+        }
+    }).sort("thank", -1).limit(30)
+    replys = random.sample(list(replys), 10)
+    return list(map(dict, replys))
+
+
+@app.post("/api/topic/info", response_model=SuccessResponse)
+async def topic_info(task: str, topic: Topic) -> SuccessResponse:
+    '''提交主题信息'''
+    topic = topic.dict()
+    replys = topic.pop('replys')
+    db.topic.update_one({'id': topic['id']}, {"$set": topic}, upsert=True)
+    # TEMP 临时排除浏览状态提交的回复（其他插件影响内容）
+    for i in replys:
+        if '<div class="show-reply">' not in i['content']:
+            db.reply.update_one({'id': i['id']}, {"$set": i}, upsert=True)
+    if (task != 'undefined'):
+        complete_task(task)
+    return SuccessResponse()
+
+
+def page_range(num: int, page_num: int = 100) -> list:
+    '''通过数量及分页数生成页码列表'''
+    return map(lambda x: x+1, range(int(bool(num % page_num)) + num//page_num))
+
+
+def new_task(id: int, page: int):
+    '''新建爬虫任务'''
+    if not db.task.find_one({'id': id, 'page': page}):
+        db.task.insert_one({
+            'id': id,
+            'page': page,
+            'distribute_time': None,
+            'complete_time': None,
+        })
+
+
+def get_task():
+    '''获取爬虫任务（分配）'''
+    task = db.task.find_one_and_update({
+        'distribute_time': None
+    }, {
+        '$set': {
+            'distribute_time': datetime.datetime.now()
+        }
+    })
+    if task:
+        url = '/t/%s?page=%s' % (task['id'], task['page'])
+        return Task(
+            sign=str(task['_id']),
+            id=task['id'],
+            page=task['page'],
+            url=url
+        )
+    else:
+        return None
+
+
+def complete_task(sign):
+    '''完成爬虫任务'''
+    db.task.find_one_and_update({
+        '_id': ObjectId(sign)
+    }, {
+        '$set': {
+            'complete_time': datetime.datetime.now()
+        }
+    })
+
+
+@app.get("/api/topic/task", response_model=Task)
+async def topic_task() -> Task:
+    '''获取爬取任务'''
+    return Task(sign='',id=0,page=0,url='')
+    task = get_task()
+    if task:
+        return task
+
+    db.info.update_one({}, {
+        '$inc': {
+            'cursor': 100
+        }
+    }, upsert=True)
+
+    # TEMP: 按顺序爬取 1 到最新主题，每次取 100 个，只爬第一页，爬完一轮后不再执行
+    latest_topic = db.topic.find_one(sort=[('id', -1)])
+    info = db.info.find_one()
+    if info['cursor'] <= latest_topic['id'] - 3000:
+        for i in range(info['cursor']-100, info['cursor']):
+            new_task(i, 1)
+
+    # 最久没更新的 10 个主题
+    topic_oldest = db.topic.find().sort("spiderTime").limit(10)
+    for i in topic_oldest:
+        for page in page_range(i['reply']):
+            new_task(i['id'], page)
+
+    # 网站最近更新 https://www.v2ex.com/changes
+    async with aiohttp.request('GET', 'https://www.v2ex.com/changes') as r:
+        topic_list = re.findall(r'/t/(\d+?)#reply(\d+)', await r.text())
+        topic_list = list(set(topic_list))  # 去重
+        for id, reply_num in topic_list:
+            id = int(id)
+            reply_num = int(reply_num)
+            topic = db.topic.find_one({'id': id})
+            # 排除评论无变化
+            if topic and topic['reply'] == reply_num:
+                continue
+
+            for page in page_range(reply_num, 100):
+                new_task(id, page)
+
+    task = get_task()
+    if task:
+        return task
+
+
+@app.post("/api/error/info", response_model=SuccessResponse)
+async def error_info(error: ErrorReport) -> SuccessResponse:
+    '''错误上报'''
+    db.error.insert_one(error.dict())
+    return SuccessResponse()
---
+++ fastapi/model.py
@@ -0,0 +1,56 @@
+
+from pydantic import BaseModel, Field, HttpUrl
+from datetime import datetime
+from typing import List, Literal, Any, Union
+
+
+class SuccessResponse(BaseModel):
+    code: int = Field(description='响应状态码', default=0)
+    msg: str = Field(description='状态码说明', default='ok')
+    data: Any = Field(description='数据')
+
+
+class Reply(BaseModel):
+    spiderTime: datetime = Field(description='爬取时间')
+    topicId: int = Field(description='主题 ID')
+    topicPage: int = Field(description='主页页码')
+    id: int = Field(description='回复 ID')
+    author: str = Field(description='作者')
+    avatar: str = Field(description='头像')
+    date: datetime = Field(description='发布日期')
+    thank: int = Field(description='感谢')
+    content: str = Field(description='评论内容')
+
+
+class Topic(BaseModel):
+    spiderTime: datetime = Field(description='爬取时间')
+    id: int = Field(description='主题 ID')
+    name: str = Field(description='标题')
+    node: str = Field(description='节点')
+    author: str = Field(description='作者')
+    avatar: str = Field(description='头像')
+    date: datetime = Field(description='发布日期')
+    reply: int = Field(description='回复数')
+    vote: int = Field(description='投票数')
+    click: int = Field(description='点击量')
+    collect: int = Field(description='收藏数')
+    thank: int = Field(description='感谢数')
+    score: int = Field(description='得分')
+
+    content: str = Field(description='正文内容')
+    append: List[str] = Field(description='追加内容')
+    replys: Union[List[Reply], None] = Field(description='获赞回复')
+
+
+class Task(BaseModel):
+    sign: str = Field(description='任务 ID')
+    id: int = Field(description='主题 ID')
+    page: int = Field(description='页码')
+    url: str = Field(description='爬取地址')
+
+
+class ErrorReport(BaseModel):
+    type: Literal['read', 'task'] = Field(description='错误类型')
+    url: str = Field(description='错误地址')
+    error: str = Field(description='错误内容')
+    time: datetime = Field(description='报错时间')
